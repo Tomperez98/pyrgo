9b82abb9a8ce441a8c94b8bd5ce30acf8ea5aae0
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2023-05-11 12:54:13 -0500

    Remove unused libraries

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,9 @@ use pyo3::prelude::*;
 
 mod pyproject;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
 /// A Python module implemented in Rust.
 #[pymodule]
 fn _pyrgo(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(pyproject::read_pyproject, m)?)?;
     Ok(())
 }
