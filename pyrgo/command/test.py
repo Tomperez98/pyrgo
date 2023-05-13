d01d30323664744da679ec981d1c18ec661bac25
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2023-05-12 19:06:21 -0500

    no rust

--- pyrgo/command/test.py
+++ pyrgo/command/test.py
@@ -21,9 +21,26 @@ from pyrgo.utilities.command import PythonExecCommand, inform_and_run_program
     default=None,
     help="Run tests based on marks. By default all tests are executed.",
 )
-def test(marker: Optional[str] = None) -> None:
+@click.option(
+    "--no-strict-markers",
+    "no_strict_markers",
+    type=bool,
+    is_flag=True,
+    default=False,
+    help="Any unknown marks applied with the `@pytest.mark.name_of_mark` decorator will trigger an error.",  # noqa: E501
+)
+def test(
+    *,
+    no_strict_markers: bool,
+    marker: Optional[str],
+) -> None:
     """Execute tests using `pytest`."""
-    args_to_add = ["tests"]
+    args_to_add = [
+        "tests",
+    ]
+    if not no_strict_markers:
+        args_to_add.append("--strict-markers")
+
     if marker:
         args_to_add.extend(["-m", marker])
 
--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-use pyo3::prelude::*;
-
-mod utilities;
-/// A Python module implemented in Rust.
-#[pymodule]
-fn _pyrgo(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(utilities::toml::ls, m)?)?;
-    Ok(())
-}
