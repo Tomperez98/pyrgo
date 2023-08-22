fa1e7badf8a24a09645ed6e5eb0c9f1d0592671a
Tomás Perez <tomasperez@192.168.1.12>
2023-08-22 16:46:29 -0500

    Public what is public

--- pyrgo/cli/cmds/__init__.py
+++ pyrgo/cli/cmds/__init__.py
@@ -1,14 +1,14 @@
 """CLI commands."""
 from __future__ import annotations
 
-from pyrgo.cli.cmds._audit import audit
-from pyrgo.cli.cmds._build import build
-from pyrgo.cli.cmds._check import check
-from pyrgo.cli.cmds._clean import clean
-from pyrgo.cli.cmds._fmt import fmt
-from pyrgo.cli.cmds._lock import lock
-from pyrgo.cli.cmds._new import new
-from pyrgo.cli.cmds._sync import sync
-from pyrgo.cli.cmds._test import test
+from pyrgo.cli.cmds.audit import audit
+from pyrgo.cli.cmds.build import build
+from pyrgo.cli.cmds.check import check
+from pyrgo.cli.cmds.clean import clean
+from pyrgo.cli.cmds.fmt import fmt
+from pyrgo.cli.cmds.lock import lock
+from pyrgo.cli.cmds.new import new
+from pyrgo.cli.cmds.sync import sync
+from pyrgo.cli.cmds.test import test
 
 __all__ = ["lock", "fmt", "check", "sync", "build", "clean", "test", "audit", "new"]
